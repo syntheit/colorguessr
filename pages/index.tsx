c918eac5880a80e653804339009ad40a0d1f0996
syntheit <daniel@matv.io>
2022-11-18 17:49:31 -0500

    Added rgb game

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,25 +1,40 @@
 import { NextPage } from "next";
 import { useState } from "react";
 import { Button } from "../components/Button/Button";
-import { Game } from "../components/Game/Game";
+import { Game_Hex } from "../components/Game/Game_Hex";
+import { Game_RGB } from "../components/Game/Game_RGB";
 
 type Props = {};
 
 const Home: NextPage<Props> = () => {
-  const [startedGame, setStartedGame] = useState(false);
+  const [selectedHex, setSelectedHex] = useState(false);
+  const [selectedRGB, setSelectedRGB] = useState(false);
+
+  // use zustand to manage game states
 
   return (
     <div className="flex items-center justify-center h-full bg-black">
-      {startedGame ? (
-        <Game />
-      ) : (
+      {selectedHex && <Game_Hex />}
+      {selectedRGB && <Game_RGB />}
+      {!selectedHex && !selectedRGB && (
         <div className="flex items-center flex-col text-white">
           <h1 className="text-5xl font-bold mb-3">ColorGuessr</h1>
           <p className="text-2xl mb-8">
             Can you the guess hex/rgb of a color just by seeing it?
           </p>
           {/* buttons for hex, rgb, etc */}
-          <Button label="Guess the hex" onClick={() => setStartedGame(true)} />
+          <div className="flex">
+            <Button
+              label="Guess the hex"
+              onClick={() => setSelectedHex(true)}
+              className="mr-4"
+            />
+            <Button
+              label="Guess the RGB"
+              onClick={() => setSelectedRGB(true)}
+              className="ml-4"
+            />
+          </div>{" "}
         </div>
       )}
     </div>
