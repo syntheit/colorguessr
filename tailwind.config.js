6644e7678fc0e3e3b1e2d2ab02bea438be9d382b
syntheit <daniel@matv.io>
2022-11-19 01:14:43 -0500

    Added fluid font sizes

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,12 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontSize: {
+        fluid: "clamp(1.5rem, 12vw - 1.5rem, 4.5rem)",
+        "fluid-lg": "clamp(3rem, 12vw - 1.5rem, 3rem)",
+      },
+    },
   },
   plugins: [],
 };
