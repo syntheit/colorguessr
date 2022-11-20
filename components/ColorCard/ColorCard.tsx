be262da24b91118d2e095b84d2960070f0843ffb
syntheit <daniel@matv.io>
2022-11-20 14:00:46 -0500

    Added diagonal comparison

--- components/ColorCard/ColorCard.tsx
+++ components/ColorCard/ColorCard.tsx
@@ -1,35 +1,52 @@
 import { NextPage } from "next";
 import { useLayoutEffect, useState } from "react";
 import { Color } from "../../constants/Color";
+import { mode } from "../../constants/types";
 
 type Props = {
-  color: Color;
-  type: "hex" | "RGB";
+  color1: { color: Color; type: mode };
+  color2: { color: Color; type: mode };
   className?: string;
 };
 
-// use diagonal design
-
-export const ColorCard: NextPage<Props> = ({ color, type, className }) => {
-  const [hex, setHex] = useState<string>();
+export const ColorCard: NextPage<Props> = ({ color1, color2, className }) => {
+  const [hex, setHex] = useState({ color1: "", color2: "" });
+  const [useLight, setUseLight] = useState({ color1: true, color2: true });
 
   useLayoutEffect(() => {
-    setHex(color.toHexString());
+    const color1_hex = color1.color.toHexString();
+    const color2_hex = color2.color.toHexString();
+    setHex({ color1: color1_hex, color2: color2_hex });
+    setUseLight({
+      color1: Color.useLight(color1.color),
+      color2: Color.useLight(color2.color),
+    });
   }, []);
 
   return (
-    <div className={["w-36 rounded-xl border-white border-solid border-4", className].join(" ")}>
-      <div
-        className={`w-full h-28 rounded-t-xl`}
-        style={{
-          backgroundColor: `rgb(${color?.r}, ${color?.g}, ${color?.b})`,
-        }}
-      ></div>
-      <div className="w-full h-12 flex items-center justify-center px-3 rounded-b-xl border-gray-900 border-solid border-2 bg-gray-900">
-        <p className="text-white text-lg font-bold">
-          {type === "hex" ? hex : color.toRGBString()}
-        </p>
-      </div>
+    <div
+      className={[
+        "w-60 h-40 flex justify-between flex-col rounded-xl border-white border-solid border-4",
+        className,
+      ].join(" ")}
+      style={{
+        backgroundImage: `linear-gradient(45deg, ${hex.color1} 50%, ${hex.color2} 50%)`,
+      }}
+    >
+      <p
+        className={`${
+          useLight.color2 ? "text-white" : "text-gray-900"
+        } text-white text-lg font-bold w-full text-right p-4`}
+      >
+        {color2.type === "hex" ? hex.color2 : color2.color.toRGBString()}
+      </p>
+      <p
+        className={`${
+          useLight.color1 ? "text-white" : "text-gray-900"
+        } text-lg font-bold w-full p-4`}
+      >
+        {color1.type === "hex" ? hex.color1 : color1.color.toRGBString()}
+      </p>
     </div>
   );
 };
